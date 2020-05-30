827b51ef5ba9da9c7600cd152a29ea1f5aa4fb55
Paweł Szostak <szostak_pawel@outlook.com>
2020-05-30 12:06:57 +0200

    Created scrolling to specific section functionality

---
+++ scroller-project/index.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const rootElement = document.querySelector('#root');
+  const sections = document.querySelectorAll('section');
+  let currentSectionIndex = 0;
+  let isThrottled = false;
+
+  document.addEventListener('wheel', e => {
+    if (isThrottled) return;
+    isThrottled = true;
+    setTimeout(() => {
+      isThrottled = false;
+    }, 1000);
+
+    const direction = e.deltaY > 0 ? 1 : -1;
+    scroll(direction);
+  });
+
+  const scroll = (direction) => {
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === sections.length - 1;
+      if (isLastSection) return;
+    } else if (direction === -1) {
+      const isFirstSection = currentSectionIndex === 0;
+      if (isFirstSection) return;
+    };
+    currentSectionIndex = currentSectionIndex + direction;
+    scrollToCurrentSection();
+  };
+
+  const scrollToCurrentSection = () => {
+    sections[currentSectionIndex].scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  };
+});
