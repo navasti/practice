8a790402c4655897ba4f53ca89bf2b73702a797b
Paweł Szostak <szostak_pawel@outlook.com>
2020-05-31 12:25:52 +0200

    Created and styled page navigation

--- scroller-project/object-oriented/Scroller.js
+++ scroller-project/object-oriented/Scroller.js
@@ -6,7 +6,7 @@ class Scroller {
     const currentSectionIndex = sectionsArray.findIndex(this.isScrolledIntoView);
     this.currentSectionIndex = Math.max(currentSectionIndex, 0);
     this.isThrottled = false;
-    this.isScrolledIntoView(this.sections[0]);
+    this.drawNavigation();
   };
 
   isScrolledIntoView = element => {
@@ -40,10 +40,41 @@ class Scroller {
   };
 
   scrollToCurrentSection = () => {
+    this.selectActiveNavItem();
     this.sections[this.currentSectionIndex].scrollIntoView({
       behavior: 'smooth',
       block: 'start'
     });
+  };
+
+  drawNavigation = () => {
+    this.navigationContainer = document.createElement('aside');
+    this.navigationContainer.setAttribute('class', 'scroller-navigation');
+    const list = document.createElement('ul');
+    this.sections.forEach((section, index) => {
+      const listItem = document.createElement('li');
+      listItem.addEventListener('click', () => {
+        this.currentSectionIndex = index;
+        this.scrollToCurrentSection();
+      });
+      list.appendChild(listItem);
+    });
+    this.navigationContainer.appendChild(list);
+    document.body.appendChild(this.navigationContainer);
+    this.selectActiveNavItem();
+  };
+
+  selectActiveNavItem = () => {
+    if (this.navigationContainer) {
+      const navigationItems = this.navigationContainer.querySelectorAll('li');
+      navigationItems.forEach((item, index) => {
+        if (index === this.currentSectionIndex) {
+          item.classList.add('active');
+        } else {
+          item.classList.remove('active');
+        };
+      });
+    };
   };
 
 };
