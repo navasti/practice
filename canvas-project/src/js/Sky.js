fcd37e48bf29bd5bfd1bd6fabb1e5aca2a567937
Paweł Szostak <szostak_pawel@outlook.com>
2020-06-29 13:02:27 +0200

    Created moving stars animation

--- canvas-project/src/js/Sky.js
+++ canvas-project/src/js/Sky.js
@@ -13,19 +13,70 @@ export default class Sky {
       this.ctx.fillRect(0, 0, this.width, this.height)
    }
 
+   drawOverlayer() {
+      const halfWidth = this.width / 2
+      const halfHeight = this.height / 2
+      const gradient = this.ctx.createRadialGradient(halfWidth, halfHeight, 250, halfWidth, halfHeight, halfWidth)
+
+      gradient.addColorStop(0, 'rgba(0, 0, 0, 0')
+      gradient.addColorStop(1, 'rgba(0, 0, 0, 0.75')
+
+      this.ctx.fillStyle = gradient
+      this.ctx.fillRect(0, 0, this.width, this.height)
+   }
+
+   generateStars(count) {
+      const stars = []
+
+      for (let i = 0; i < count; i++) {
+         const radius = Math.random() * 3 + 2
+         stars.push({
+            x: (Math.random() * this.width),
+            y: (Math.random() * this.height),
+            speed: Math.random() + 0.25,
+            initialRadius: radius,
+            color: '#fff',
+            radius
+         })
+      }
+
+      this.stars = stars
+   }
+
+   drawStars() {
+      this.stars.forEach(star => {
+         this.drawStar(star)
+      })
+   }
+
+   updateStars() {
+      this.stars.forEach(star => {
+         star.x += star.speed
+         star.y -= star.speed * ((this.width / 2) - star.x) / 3000
+         star.radius = star.initialRadius * (Math.random() / 3 + 0.9)
+
+         if (star.x > this.width + 2 * star.radius) {
+            star.x = -2 * star.radius
+         }
+      })
+   }
+
+   clearCanvas() {
+      this.ctx.fillStyle = '#000'
+      this.ctx.fillRect(0, 0, this.width, this.height)
+   }
 
    drawStar(star) {
       this.ctx.save()
-
       this.ctx.fillStyle = star.color
       this.ctx.beginPath()
       this.ctx.translate(star.x, star.y)
       this.ctx.moveTo(0, 0 - star.radius)
 
       for (let i = 0; i < 5; i++) {
-         this.ctx.rotate(Math.PI / 180 * 36)
-         this.ctx.lineTo(0, 0 - star.radius * 0.45)
-         this.ctx.rotate(Math.PI / 180 * 36)
+         this.ctx.rotate((Math.PI / 180) * 36)
+         this.ctx.lineTo(0, 0 - star.radius * 0.55)
+         this.ctx.rotate((Math.PI / 180) * 36)
          this.ctx.lineTo(0, 0 - star.radius)
       }
 
@@ -34,18 +85,20 @@ export default class Sky {
    }
 
    draw() {
+      this.clearCanvas()
+
+      this.drawStars()
+      this.updateStars()
+
+      this.drawOverlayer()
+
       window.requestAnimationFrame(() => this.draw())
    }
 
    run() {
       this.initCanvas()
-      // this.draw()
-      this.drawStar({
-         x: 100,
-         y: 100,
-         color: '#31abef',
-         radius: 100
-      })
+      this.generateStars(500)
+      this.draw()
    }
 
 }
