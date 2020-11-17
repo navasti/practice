17b12508e4a36082a3f1b0f1ebf425f6bc97d5a1
Paweł Szostak <szostak_pawel@outlook.com>
2020-11-17 14:25:49 +0100

    Created app logic

---
+++ react-typescript-quiz/src/components/QuestionCard.tsx
@@ -0,0 +1,46 @@
+import React, { MouseEvent } from "react";
+
+// Types
+import { AnswerObject } from "../App";
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: (e: MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => {
+  return (
+    <div>
+      <p className="number">
+        Question: {questionNr} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers.map(answer => (
+          <div key={answer}>
+            <button
+              disabled={userAnswer ? true : false}
+              value={answer}
+              onClick={callback}
+            >
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
