83a6e111885842c34bfcc519de06c39a058cca60
Pawel Szostak <szostak_pawel@outlook.com>
2021-03-02 23:19:42 +0100

    Finish app

--- page-transitions-react/src/App.js
+++ page-transitions-react/src/App.js
@@ -1,7 +1,44 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.scss";
+import { AnimatePresence } from "framer-motion";
+
+//Pages
+import Home from "./pages/home";
+import Model from "./pages/model";
+
+//components
+import Header from "./components/header";
 
 const App = () => {
-  return <h1>hell0</h1>;
+  const imageDetails = {
+    width: 524,
+    height: 650,
+  };
+
+  return (
+    <Router>
+      <Header />
+      <Route
+        render={({ location }) => (
+          <AnimatePresence initial={true} exitBeforeEnter>
+            <Switch location={location} key={location.pathname}>
+              <Route
+                exact
+                path="/"
+                render={() => <Home imageDetails={imageDetails} />}
+              />
+              <Route
+                exact
+                path="/model/:id"
+                render={() => <Model imageDetails={imageDetails} />}
+              />
+            </Switch>
+          </AnimatePresence>
+        )}
+      />
+    </Router>
+  );
 };
 
 export default App;
